01bfd710225721aed7cead8ccd5acc99d08cb59b
ChristopherSamso <chsa6996@colorado.edu>
2022-10-13 10:29:55 -0600

    Initial Commit

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Christopher Samso on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
